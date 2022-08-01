93025b8a37c58ab1b147e1c88ed0a36b4c91552f
Danek <danek.fill@outlook.com>
2022-08-01 16:48:43 -0400

    Add edit saved login page and route

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 const registerRoutes = require("./routes/register");
 const addNewRoutes = require("./routes/addNew");
+const editRoutes = require("./routes/edit");
 const loginRoutes = require("./routes/login");
 
 
@@ -47,6 +48,7 @@ app.use("/api/users", usersRoutes(db));
 
 app.use("/register", registerRoutes());
 app.use("/addNewLogin", addNewRoutes());
+app.use("/edit", editRoutes());
 app.use("/login", loginRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
