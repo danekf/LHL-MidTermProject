9cbff250af99aa2ee31828a2e9d17e5a76ac0db4
Danek <danek.fill@outlook.com>
2022-08-01 14:18:19 -0400

    Update users table with all required info, update seed with 2 examples

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (email, username, password, first_name, last_name) VALUES ('alice@wonderland.ca','bunnyluv','password','Alice', 'Kingsleigh');
+
+INSERT INTO users (email, username, password, first_name, last_name, org_id)
+VALUES ('kira@live.ca', 'purplemonster', 'password','Kira', "D'emeraude",'0');
