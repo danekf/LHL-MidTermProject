96011c48f05a4ee0ec2ef6f45e46ab5614c0282d
Danek <danek.fill@outlook.com>
2022-08-03 18:49:51 -0400

    cleanup

--- public/scripts/copyToClipboard.js
+++ public/scripts/copyToClipboard.js
@@ -1,11 +1,10 @@
 
 $(document).ready(() => {
+
   $('#copyPasswordToClipboard').on('click',
   (event) => {
     let password = $("#password-field");
     $(password).select();
     document.execCommand("copy");
-
-
   })
 });
