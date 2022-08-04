1b55fb76803c04635e7227d511b6d9ec4da62e32
Danek <danek.fill@outlook.com>
2022-08-04 19:58:13 -0400

    implement copy to clipboard for targetted logins

--- public/scripts/copyToClipboard.js
+++ public/scripts/copyToClipboard.js
@@ -1,18 +1,49 @@
 
 $(document).ready(() => {
 
-  $('.username').on('click',
+  $('#copyPasswordToClipboard').on('click',
   (event) => {
-    let password = $(event.target).dosomething;
+    let password = $("#password-field");
     $(password).select();
     document.execCommand("copy");
   })
 
+  $('.username').on('click',
+  (event) => {
+    let username = $(event.target).text().slice(10);
+
+    username = username.replace(/\s+/g, ''); //remove whitespace
+
+    var temp = document.createElement("textarea");
+
+    //create temp body element to hold value
+    document.body.appendChild(temp);
+    temp.value = username;
+    temp.select();
+    document.execCommand("copy");
+    //remove temp body element
+    document.body.removeChild(temp);
+
+    alert("Username copied to clipboard.");
+
+  })
+
   $('.password').on('click',
   (event) => {
-    let password = $(event.target).dosomething;
-    $(password).select();
+    let password = $(event.target).text().slice(10);
+    password = password.replace(/\s+/g, ''); //remove whitespace
+
+    var temp = document.createElement("textarea");
+
+    //create temp body element to hold value
+    document.body.appendChild(temp);
+    temp.value = password;
+    temp.select();
     document.execCommand("copy");
+    //remove temp body element
+    document.body.removeChild(temp);
+
+    alert("Password copied to clipboard.");
   })
 
 
